d445f0d0455353e084330b27a378f9e62d66c275
mlogic1 <multilogic@outlook.com>
2018-11-18 13:27:17 +0100

    - added a floating action button (test)

--- app/src/main/java/com/merodyadt/belotasistent/ActivityMain.java
+++ app/src/main/java/com/merodyadt/belotasistent/ActivityMain.java
@@ -1,8 +1,10 @@
 package com.merodyadt.belotasistent;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class ActivityMain extends AppCompatActivity {
@@ -11,6 +13,16 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Hello floating buton", "Test");
+            }
+        });
     }
 
     public void StartScoreboard(View view){
